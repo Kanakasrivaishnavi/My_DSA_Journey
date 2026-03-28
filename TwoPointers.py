4a235f203742e006dd6e35a0453883c9ec25d1c3
kanaka sri vaishnavi <kukkalakanakasrivaishnavi@gmail.com>
2026-03-28 13:16:21 +0530

    Enhance comments for min_sub_arr function
    Added problem description and efficiency analysis for the two-pointer technique used in finding the smallest subarray with a sum >= target.

--- TwoPointers.py
+++ TwoPointers.py
@@ -1,7 +1,12 @@
-
+#Problem: Find smallest subarray with sum >= target.
 #Two pointer technique
 #time complexity - O(n)
-
+#Each element is processed at most twice, once when added to the window and once when removed, making the solution linear in time. 
+#Space Complexity: O(1)
+          #The solution only uses a fixed amount of extra space for variables like start, sum, and minLen, so the space complexity is constant.
+#Efficiency: Efficient for problems involving finding subarrays with specific sums or lengths, as it avoids recomputing sums multiple times.
+#Best For:   Problems where you need to find the minimum or maximum subarray length satisfying certain conditions (like
+             sum >= target)
 
 def min_sub_arr(arr,target):
     start = 0
@@ -13,12 +18,8 @@ def min_sub_arr(arr,target):
         while  current_sum >= target: # loop execute only if condition is satisfied 
                 length = end - start + 1 
                 min_len = min(min_len,length) #find min length
-                current_sum -= arr[start] 
-"""
-subtract the value in the start index then current_sum
-sum value will get update and move to condition in while loop and it check if 
-it is true then loop will get execute else end will update
-"""                
+                current_sum -= arr[start] #subtract the value in the start index then current_sum sum value will get update and 
+                                          #move to condition in while loop and it check if it is true then loop will get execute else end will update               
                 start += 1  #increase start value (move to right)
                 
     if min_len == float('inf'):
